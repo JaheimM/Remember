f3b43c12c874f0a5ebaaa2083a4632e93d2ff3e2
JaheimM <84516545+JaheimM@users.noreply.github.com>
2021-08-13 14:18:12 -0400

    Update openingCinematic.js

--- openingCinematic.js
+++ openingCinematic.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if ($(dialogue).text() == object.myDialogue[14]) {
         $(content).fadeOut(1 * 1000);
         setTimeout(function () {
-          window.location = 'index.html';
+          window.location = 'Index.html';
           return false;
         }, 1000);
         
